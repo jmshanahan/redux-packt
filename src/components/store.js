807399705fa9ecc7cf8a617acd2b0d8e84335059
jmshanahan <comeraghsolutions@gail.com>
2020-02-18 11:24:23 +0000

    something is wrong

--- src/components/store.js
+++ src/components/store.js
@@ -1,4 +1,12 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers";
 const store = createStore(rootReducer);
+
+// function interruptDispatch(action) {
+//   console.log("before dispatching");
+//   store.dispatch(action);
+//   console.log("after dispatching");
+// }
+
+// interruptDispatch({ order: "Tea", amount: 1 });
 export default store;
